751496d484d6d8235db2cef5bd33a0e6c2ddc6f6
Victor Pudeyev <piousbox@gmail.com>
2012-06-15 14:46:50 -0700

    some layout fixx

--- config/routes.rb
+++ config/routes.rb
@@ -7,34 +7,28 @@ Opcoes::Application.routes.draw do
   get 'login' => 'pages#login'
   post 'do-login' => 'pages#do_login'
   get 'logout' => 'pages#logout'
+  
+  # pages
   get 'about-us' => 'pages#about_us'
   get 'privacy-policy' => 'pages#privacy'
   
   # register manager
-  
   post 'manager/process' => 'manager#process_registration'
   post 'manager/process_2' => 'manager#process_registration_2'
-  
   get 'manager/register' => 'manager#register'
   get 'manager/register_2' => 'manager#register_2'
-  
   get 'manager/dashboard' => 'manager#dashboard'
   get 'managers' => 'manager#index'
   
   # register builder
-  
   get 'builder/register' => 'builder#register'
   post 'builder/process' => 'builder#process_registration'  
-  
   get 'builder/dashboard' => 'builder#dashboard'
   get 'builder/promote' => 'builder#promote'
   post 'builder/process_promote' => 'builder#process_promote'
-  
   get 'builders' => 'builder#index'
    
-  
   # projects
-  
   get 'projects' => 'projects#index'
   get 'projects/new' => 'projects#new'
   post 'projects' => 'projects#create'
