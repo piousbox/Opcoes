ef60235b0affefdc6e6c3eb52d04ba02e95921c5
Victor Pudeyev <piousbox@gmail.com>
2012-06-15 17:41:04 -0700

    working on styling and jquery fancy effects on homepage

--- app/assets/javascripts/menus.js
+++ app/assets/javascripts/menus.js
@@ -1,64 +1,66 @@
 
 $(document).ready(function() {
   
-  var menu_register_can_collapse = true
- 
-  $(".menu_register_hover").hoverIntent({
-    sensitivity: 1,
-    interval: 50,
-    over: function() {
-      $('.menu_register_content').slideDown()
-    },
-    timeout: 1300,
-    out: function() {
-      if (menu_register_can_collapse) {
-        $('.menu_register_content').slideUp()
-      }
-    }
-  })
-  
-  $(".menu_register_content").hoverIntent({
-    sensitivity: 1,
-    interval: 50,
-    over: function() {
-      menu_register_can_collapse = false
-    },
-    timeout: 1300,
-    out: function() {
-      menu_register_can_collapse = true
-      $('.menu_register_content').slideUp()
-    }
-  })
-  
-  // tour only
-  
-  var menu_register_can_collapse_2 = true
- 
-  $(".menu_register_hover_2").hoverIntent({
-    sensitivity: 1,
-    interval: 50,
-    over: function() {
-      $('.menu_register_content_2').slideDown()
-    },
-    timeout: 1300,
-    out: function() {
-      if (menu_register_can_collapse_2) {
-        $('.menu_register_content_2').slideUp()
-      }
-    }
-  })
-  
-  $(".menu_register_content_2").hoverIntent({
-    sensitivity: 1,
-    interval: 50,
-    over: function() {
-      menu_register_can_collapse_2 = false
-    },
-    timeout: 1300,
-    out: function() {
-      menu_register_can_collapse_2 = true
-      $('.menu_register_content_2').slideUp()
-    }
-  })
+//  var menu_register_can_collapse = true
+// 
+//  $(".menu_register_hover").hoverIntent({
+//    sensitivity: 1,
+//    interval: 50,
+//    over: function() {
+//      $('.menu_register_content').slideDown()
+//    },
+//    timeout: 1300,
+//    out: function() {
+//      if (menu_register_can_collapse) {
+//        $('.menu_register_content').slideUp()
+//      }
+//    }
+//  })
+//  
+//  $(".menu_register_content").hoverIntent({
+//    sensitivity: 1,
+//    interval: 50,
+//    over: function() {
+//      menu_register_can_collapse = false
+//    },
+//    timeout: 1300,
+//    out: function() {
+//      menu_register_can_collapse = true
+//      $('.menu_register_content').slideUp()
+//    }
+//  })
+//  
+//  // tour only
+//  
+//  var menu_register_can_collapse_2 = true
+// 
+//  $(".menu_register_hover_2").hoverIntent({
+//    sensitivity: 1,
+//    interval: 50,
+//    over: function() {
+//      $('.menu_register_content_2').slideDown()
+//    },
+//    timeout: 1300,
+//    out: function() {
+//      if (menu_register_can_collapse_2) {
+//        $('.menu_register_content_2').slideUp()
+//      }
+//    }
+//  })
+//  
+//  $(".menu_register_content_2").hoverIntent({
+//    sensitivity: 1,
+//    interval: 50,
+//    over: function() {
+//      menu_register_can_collapse_2 = false
+//    },
+//    timeout: 1300,
+//    out: function() {
+//      menu_register_can_collapse_2 = true
+//      $('.menu_register_content_2').slideUp()
+//    }
+//  })
+//  
+
 })
 
